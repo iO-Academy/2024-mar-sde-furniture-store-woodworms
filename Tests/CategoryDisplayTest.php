76fbc8b50b69a733b7b37277bfc5ae30cf51f775
Kate <kate.wileman@hotmail.co.uk>
2024-05-21 11:23:15 +0100

    story 2 unit testing for review

--- Tests/CategoryDisplayTest.php
+++ Tests/CategoryDisplayTest.php
@@ -1,26 +1,38 @@
 <?php
 
-require_once '../src/Services/CategoryDisplayService.php';
-
+require_once 'src/Services/CategoryDisplayService.php';
 use PHPUnit\Framework\TestCase;
 
-class CategoryDisplayTest extends TestCase {
-
-    public function testDisplayByCategory() // success
+class CategoryDisplayTest extends TestCase
+{
+    public function testDisplayByCategoryMock()
     {
-        $result = displayByCategory('chair', '45');
-        $this->assertSame('chair', ' 45', $result);
+        $categoryMock = $this->createMock(Category::class);
+        $categoryMock->name='Fridge';
+        $categoryMock->total_stock=100;
+        $result = CategoryDisplayService::displayByCategory($categoryMock);
+        $expectedResult = '<div class="flex justify-between items-center bg-slate-100 p-5">
+            <h3 class="text-2xl">Fridge</h3>
+            <span class="bg-teal-500 text-2xl px-2 py-1 rounded">100</span>
+            </div>';
+        $this->assertSame($expectedResult, $result);
     }
 
-//    public function testCalculateAreaOfCircleNegative() // failure
-//    {
-//        $this->expectException(Exception::class);
-//        calculateAreaOfCircle(-5);
-//    }
-//    public function testCalculateAreaOfCircleWrongDataType() // malformed
-//    {
-//        $this->expectException(TypeError::class);
-//        calculateAreaOfCircle('Mike');
-//
-//    }
+    public function testDisplayByCategoryFailure()
+    {
+        $categoryMock = $this->createMock(Category::class);
+        $categoryMock->name='Fridge';
+        $categoryMock->total_stock=-10;
+        $this->expectException(Exception::class);
+        CategoryDisplayService::displayByCategory($categoryMock);
+    }
+
+    public function testDisplayByCategoryMalformed()
+    {
+        $categoryMock = $this->createMock(Category::class);
+        $categoryMock->name=10;
+        $categoryMock->total_stock='Fridge';
+        $this->expectException(TypeError::class);
+        CategoryDisplayService::displayByCategory($categoryMock);
+    }
 }
--- src/Services/CategoryDisplayService.php
+++ src/Services/CategoryDisplayService.php
@@ -1,15 +1,23 @@
+
 <?php
 
 require_once 'src/Entities/Category.php';
 
 class CategoryDisplayService
 {
-    public static function displayByCategory($name, $total_stock)
+    public static function displayByCategory($category): string
     {
-        return '<div class="flex justify-between items-center bg-slate-100 p-5">
-        <h3 class="text-2xl">' . $name . '</h3>
-        <span class="bg-teal-500 text-2xl px-2 py-1 rounded">' . $total_stock . '</span>
-        </div>';
+        if (!is_int($category->total_stock) || !is_string($category->name)) {
+            throw new TypeError('Invalid Type');
+        }
+        else if ($category->total_stock < 0){
+            throw new Exception('Total stock can\'t be less than 0');
+        }
+        else{
+            return '<div class="flex justify-between items-center bg-slate-100 p-5">
+            <h3 class="text-2xl">' . $category->name . '</h3>
+            <span class="bg-teal-500 text-2xl px-2 py-1 rounded">' . $category->total_stock . '</span>
+            </div>';
+        }
     }
-
 }
